5025cf5223f676e44812d466dc2e9da9eaf2c7dc
Tyler <tylersayvetz@gmail.com>
2019-12-20 15:53:02 -0800

    reconfigure mysql connection config

--- seed_database_with_users.js
+++ seed_database_with_users.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'roadrat',
-	database: 'store'
+	database: 'store',
+	insecureAuth: true,
 });
 
 connection.connect();
